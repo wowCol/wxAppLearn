8276d40cea5b8101f76a6f82399f9327eb104135
wowCol <wqy2693699654@qq.com>
2025-06-04 16:12:43 +0800

    轮播图数据获取

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入网络请求包
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+$http.baseUrl = 'https://www.uinav.com'
+
+// 配置请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 配置响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -5,7 +5,28 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      // 轮播表数据数组
+      swiperList: []
+    };
+  },
+  onLoad() {
+    this.getSwiperList();
+  },
+  methods: {
+    async getSwiperList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata');
+      // 请求失败
+      if (res.meta.status !== 200) {
+        return uni.showToast({
+          title: '数据请求失败',
+          duration: 1500,
+          icon: 'none'
+        });
+      }
+
+      this.swiperList = res.message;
+    }
   }
 };
 </script>
