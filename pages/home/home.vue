2b81b46af12fe37f10a921d6f84811800071e221
wowCol <wqy2693699654@qq.com>
2025-06-04 17:02:16 +0800

    分类导航前端与tarbar栏跳转

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,12 +1,21 @@
 <template>
-  <!-- 轮播图区域 -->
-  <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
-    <swiper-item v-for="(item, index) in swiperList" :key="index">
-      <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
-        <image :src="item.image_src" mode=""></image>
-      </navigator>
-    </swiper-item>
-  </swiper>
+  <view>
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+      <swiper-item v-for="(item, index) in swiperList" :key="index">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src" mode=""></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+
+    <!-- 分类导航区域 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, index) in navList" :key="index" @click="navClickHandler(item)">
+        <image :src="item.image_src" mode="" class="nav-img"></image>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
@@ -14,11 +23,14 @@ export default {
   data() {
     return {
       // 轮播表数据数组
-      swiperList: []
+      swiperList: [],
+      // 分类导航栏数据
+      navList: []
     };
   },
   onLoad() {
     this.getSwiperList();
+    this.getNavList();
   },
   methods: {
     async getSwiperList() {
@@ -30,6 +42,22 @@ export default {
 
       this.swiperList = res.message;
       // uni.$showMsg('成功获得数据');
+    },
+    async getNavList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/catitems');
+      // 请求失败
+      if (res.meta.status !== 200) {
+        return uni.$showMsg;
+      }
+
+      this.navList = res.message;
+    },
+    navClickHandler(item) {
+      if (item.name === '分类') {
+        uni.switchTab({
+          url: '/pages/cate/cate'
+        });
+      }
     }
   }
 };
@@ -43,6 +71,17 @@ swiper {
   image {
     width: 100%;
     height: 100%;
+  }
+}
+
+.nav-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
+
+  .nav-img {
+    width: 120rpx;
+    height: 140rpx;
   }
 }
 </style>
