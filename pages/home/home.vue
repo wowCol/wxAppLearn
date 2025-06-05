673086ca0bb75bc0849318a1a95215e89bfe96a1
wowCol <wqy2693699654@qq.com>
2025-06-05 09:22:09 +0800

    分类导航跳转与子包创建

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -15,6 +15,29 @@
         <image :src="item.image_src" mode="" class="nav-img"></image>
       </view>
     </view>
+
+    <!-- 楼层区域 -->
+    <!-- 楼层的容器 -->
+    <view class="floor-list">
+      <!-- 每一个楼层的 item 项 -->
+      <view class="floor-item" v-for="(item, index) in floorList" :key="index">
+        <!-- 楼层的标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层的图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片的盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{ width: item.product_list[0].image_width + 'rpx' }" mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧4个小图片的盒子 -->
+          <view class="right-img-box">
+            <navigator class="right-img-item" v-for="(item2, index2) in item.product_list" :key="index2" v-if="index2 !== 0" :url="item2.url">
+              <image :src="item2.image_src" :style="{ width: item2.image_width + 'rpx' }" mode="widthFix"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -25,12 +48,15 @@ export default {
       // 轮播表数据数组
       swiperList: [],
       // 分类导航栏数据
-      navList: []
+      navList: [],
+      // 楼层数据
+      floorList: []
     };
   },
   onLoad() {
     this.getSwiperList();
     this.getNavList();
+    this.getFloorList();
   },
   methods: {
     async getSwiperList() {
@@ -51,6 +77,21 @@ export default {
       }
 
       this.navList = res.message;
+    },
+    async getFloorList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/home/floordata');
+      // 请求失败
+      if (res.meta.status !== 200) {
+        return uni.$showMsg;
+      }
+
+      res.message.forEach((floor) => {
+        floor.product_list.forEach((prod) => {
+          prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1];
+        });
+      });
+
+      this.floorList = res.message;
     },
     navClickHandler(item) {
       if (item.name === '分类') {
@@ -83,5 +124,21 @@ swiper {
     width: 120rpx;
     height: 140rpx;
   }
+}
+
+.floor-title {
+  width: 100%;
+  height: 60rpx;
+}
+
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
 }
 </style>
