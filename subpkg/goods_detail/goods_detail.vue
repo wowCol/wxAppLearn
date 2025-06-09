bcd7f9f0bd70edf15642325bb42b92e6ed15f586
wowCol <wqy2693699654@qq.com>
2025-06-09 16:40:10 +0800

    商品详细页面基础数据获取

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,22 +1,28 @@
 <template>
-  <view>
-    
-  </view>
+  <view></view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      
+export default {
+  data() {
+    return {
+      goods_info: {}
+    };
+  },
+  onLoad(options) {
+    const goods_id = options.goods_id;
+    this.getGoodsDetail(goods_id);
+  },
+  methods: {
+    async getGoodsDetail(goods_id) {
+      const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id });
+
+      if (res.meta.status !== 200) return uni.$showMsg();
+
+      this.goods_info = res.message;
     }
   }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
