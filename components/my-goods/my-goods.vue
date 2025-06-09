19742f37fe2e8c476532843d9a5f7ffe007bc52f
wowCol <wqy2693699654@qq.com>
2025-06-09 16:12:34 +0800

    商品列表功能完善，上拉加载、下拉刷新与跳转商品详细页面

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -12,7 +12,7 @@
           {{ goods.goods_name }}
         </view>
         <view class="goods-info-box">
-          <view class="goods-price">¥{{ goods.goods_price }}</view>
+          <view class="goods-price">¥{{ goods.goods_price | tofixed }}</view>
         </view>
       </view>
     </view>
@@ -25,13 +25,19 @@ export default {
   props: {
     goods: {
       type: Object,
-      default: {}
+      default: () => ({})
     }
   },
   data() {
     return {
       defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
     };
+  },
+  filters: {
+    // 把数字处理为带两位小数的数字
+    tofixed(num) {
+      return Number(num).toFixed(2);
+    }
   }
 };
 </script>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,9 +1,9 @@
 <template>
   <view>
     <view class="goods-list">
-      <block v-for="(goods, index) in goodsList" :key="index">
+      <view v-for="(goods, index) in goodsList" :key="index" @click="gotoDetail(goods)">
         <my-goods :goods="goods"></my-goods>
-      </block>
+      </view>
     </view>
   </view>
 </template>
@@ -20,7 +20,9 @@ export default {
         pagesize: 10
       },
       goodsList: [],
-      total: 0
+      total: 0,
+      // 设置节流阀
+      isLoading: false
     };
   },
   onLoad(options) {
@@ -30,13 +32,45 @@ export default {
     this.getGoodsList();
   },
   methods: {
-    async getGoodsList() {
+    async getGoodsList(cb) {
+      this.isLoading = true;
+
       const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+      // 调用回调函数
+      cb && cb();
+
       if (res.meta.status !== 200) return uni.$showMsg();
 
-      this.goodsList = res.message.goods;
+      this.goodsList = [...this.goodsList, ...res.message.goods];
+
       this.total = res.message.total;
+
+      this.isLoading = false;
+    },
+    gotoDetail(goods) {
+      uni.navigateTo({
+        url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+      });
     }
+  },
+  onReachBottom() {
+    if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕');
+
+    if (this.isLoading) return;
+
+    // 自动让页码自增1
+    this.queryObj.pagenum++;
+    this.getGoodsList();
+  },
+  onPullDownRefresh() {
+    // 重置关键数据
+    this.queryObj.pagenum = 1;
+    this.total = 0;
+    this.isLoading = false;
+    this, (goodsList = []);
+
+    // 重新发起数据请求
+    this.getGoodsList(() => uni.stopPullDownRefresh());
   }
 };
 </script>
