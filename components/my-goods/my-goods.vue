88cfb5a6a5c9ae097d9979f13df64a892d70c3bc
wowCol <wqy2693699654@qq.com>
2025-06-09 14:26:36 +0800

    将单个列表项封装为组件

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,73 @@
+<template>
+  <view>
+    <view class="goods-item">
+      <!-- 左侧的盒子 -->
+      <view class="goods-item-left">
+        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+      </view>
+      <!-- 右侧的盒子 -->
+      <view class="goods-item-right">
+        <!-- 商品的名字 -->
+        <view class="goods-name">
+          {{ goods.goods_name }}
+        </view>
+        <view class="goods-info-box">
+          <view class="goods-price">¥{{ goods.goods_price }}</view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'my-goods',
+  props: {
+    goods: {
+      type: Object,
+      default: {}
+    }
+  },
+  data() {
+    return {
+      defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.goods-item {
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+
+  .goods-item-left {
+    margin-left: 5px;
+
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-left: 5px;
+
+    .goods-name {
+      font-size: 13px;
+    }
+
+    .goods-info-box {
+      .goods-price {
+        color: #c00000;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -2,22 +2,7 @@
   <view>
     <view class="goods-list">
       <block v-for="(goods, index) in goodsList" :key="index">
-        <view class="goods-item">
-          <!-- 左侧的盒子 -->
-          <view class="goods-item-left">
-            <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
-          </view>
-          <!-- 右侧的盒子 -->
-          <view class="goods-item-right">
-            <!-- 商品的名字 -->
-            <view class="goods-name">
-              {{ goods.goods_name }}
-            </view>
-            <view class="goods-info-box">
-              <view class="goods-price">¥{{ goods.goods_price }}</view>
-            </view>
-          </view>
-        </view>
+        <my-goods :goods="goods"></my-goods>
       </block>
     </view>
   </view>
@@ -35,8 +20,7 @@ export default {
         pagesize: 10
       },
       goodsList: [],
-      total: 0,
-      defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      total: 0
     };
   },
   onLoad(options) {
@@ -57,38 +41,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.goods-item {
-  display: flex;
-  padding: 10px 5px;
-  border-bottom: 1px solid #f0f0f0;
-
-  .goods-item-left {
-    margin-left: 5px;
-
-    .goods-pic {
-      width: 100px;
-      height: 100px;
-      display: block;
-    }
-  }
-
-  .goods-item-right {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding-left: 5px;
-
-    .goods-name {
-      font-size: 13px;
-    }
-
-    .goods-info-box {
-      .goods-price {
-        color: #c00000;
-        font-size: 16px;
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
